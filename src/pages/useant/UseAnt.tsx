b484a3c1430443d9cba423dec0aa439c88b9d6e0
jonathan <jonathanxavier556@gmail.com>
2025-02-21 00:27:54 -0300

    criando um form

---
+++ src/pages/useant/UseAnt.tsx
@@ -0,0 +1,17 @@
+import { Button, Flex, Input } from "antd"
+
+const UseAnt: React.FC = () => {
+    return(
+        <Flex >
+
+            <Input
+
+            />
+
+            <Button>Adicionar</Button>
+
+        </Flex>
+    )
+}
+
+export default UseAnt
